71eed93bf71b318899eef7eab4aac361b0d51f54
Lekni <loswinnorvell@gmail.com>
2024-07-13 22:38:45 +0200

    Update prenom_generateur.py

--- prenom_generateur.py
+++ prenom_generateur.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import random
+import re
 
-# Listes de prénoms (exemples étendus)
+# Listes de prénoms fictives
 prenoms_fr_masc = ["Alexandre", "Benjamin", "Charles", "David", "Émile", "François", "Gabriel", "Henri", "Isaac", "Jacques", "Laurent", "Martin", "Nicolas", "Olivier", "Paul", "Quentin", "Romain", "Simon", "Thomas", "Ulysse", "Vincent", "William", "Xavier", "Yves", "Zacharie"]
 prenoms_fr_fem = ["Amandine", "Bérénice", "Charlotte", "Delphine", "Émilie", "Florence", "Gabrielle", "Hélène", "Isabelle", "Julie", "Karine", "Laure", "Marie", "Nathalie", "Olivia", "Pauline", "Quitterie", "Rachel", "Sophie", "Thérèse", "Ursule", "Valérie", "Wendy", "Xénia", "Yvette", "Zoé"]
 prenoms_de_masc = ["Alexander", "Benjamin", "Carl", "David", "Emil", "Friedrich", "Georg", "Heinrich", "Isaak", "Jakob", "Ludwig", "Martin", "Niklas", "Otto", "Paul", "Quirin", "Rudolf", "Stefan", "Thomas", "Ulrich", "Viktor", "Werner", "Xaver", "Yannick", "Zacharias"]
@@ -13,56 +14,54 @@ prenoms_es_fem = ["Amanda", "Beatriz", "Carlota", "Diana", "Emilia", "Fernanda",
 prenoms_is_masc = ["Alexander", "Bjorn", "Carl", "David", "Emil", "Finnur", "Gunnar", "Halldor", "Ingvar", "Jon", "Kristjan", "Leifur", "Magnus", "Njall", "Olafur", "Petur", "Quinn", "Ragnar", "Stefan", "Thor", "Ulfr", "Valdimar", "William", "Xavier", "Ymir", "Zacharias"]
 prenoms_is_fem = ["Amanda", "Björk", "Carla", "Dóra", "Elísa", "Freya", "Gudrun", "Hildur", "Ingibjörg", "Jóna", "Katrin", "Lilja", "Margret", "Nína", "Olga", "Pálína", "Quinn", "Rakel", "Sara", "Tinna", "Urður", "Vigdís", "Wendy", "Xena", "Ylfa", "Zoe"]
 
-# Fonction pour segmenter les prénoms par syllabes (simplifiée)
+# Fonction de segmentation syllabique
 def segmenter_syllabes(prenom):
-    syllabes = []
-    voyelles = "aeiouyAEIOUY"
-    syllabe = ""
-    for lettre in prenom:
-        syllabe += lettre
-        if lettre in voyelles:
-            syllabes.append(syllabe)
-            syllabe = ""
-    if syllabe:
-        syllabes.append(syllabe)
-    return syllabes
+    return re.findall(r'[^aeiouy]*[aeiouy]+(?:[^aeiouy]*$|[^aeiouy](?=[^aeiouy]))?', prenom, re.IGNORECASE)
 
-# Regroupement des prénoms par langue et genre
-prenoms = {
-    "Français Masculin": prenoms_fr_masc,
-    "Français Féminin": prenoms_fr_fem,
-    "Allemand Masculin": prenoms_de_masc,
-    "Allemand Féminin": prenoms_de_fem,
-    "Anglais Masculin": prenoms_en_masc,
-    "Anglais Féminin": prenoms_en_fem,
-    "Espagnol Masculin": prenoms_es_masc,
-    "Espagnol Féminin": prenoms_es_fem,
-    "Islandais Masculin": prenoms_is_masc,
-    "Islandais Féminin": prenoms_is_fem,
-}
+# Fonction pour générer des prénoms
+def generer_prenom(liste_prenoms, nb_syllabes, nb_lettres):
+    syllabes = []
+    for prenom in liste_prenoms:
+        syllabes.extend(segmenter_syllabes(prenom))
+    
+    syllabes = list(set(syllabes))
+    
+    generated_prenoms = []
+    while len(generated_prenoms) < 5:
+        prenom = ''.join(random.choices(syllabes, k=nb_syllabes))
+        if len(prenom) <= nb_lettres:
+            generated_prenoms.append(prenom.capitalize())
+    
+    return generated_prenoms
 
 # Interface Streamlit
 st.title("Générateur de Prénoms")
-langue_genre = st.selectbox("Choisissez la langue et le genre", list(prenoms.keys()))
-nb_syllabes = st.slider("Nombre de syllabes", 1, 5, 2)
-nb_lettres = st.slider("Nombre de lettres", 3, 10, 6)
-nb_prenoms = st.slider("Nombre de prénoms à générer", 5, 50, 10)
 
-# Bouton pour générer les prénoms
-if st.button("Générer les prénoms"):
-    # Segmenter les prénoms de la liste choisie
-    prenoms_segmentes = [segmenter_syllabes(p) for p in prenoms[langue_genre]]
+# Sélection de la langue et du genre
+langue = st.selectbox("Choisissez une langue", ["Français", "Allemand", "Anglais", "Espagnol", "Islandais"])
+genre = st.selectbox("Choisissez un genre", ["Masculin", "Féminin"])
 
-    # Générer des prénoms selon les critères
-    prenoms_generes = []
-    while len(prenoms_generes) < nb_prenoms:
-        prenom = random.choice(prenoms[langue_genre])
-        syllabes = segmenter_syllabes(prenom)
-        if len(syllabes) == nb_syllabes and len(prenom) == nb_lettres:
-            prenoms_generes.append(prenom)
+# Sélection du nombre de syllabes et de lettres
+nb_syllabes = st.slider("Nombre de syllabes", min_value=1, max_value=5, value=2)
+nb_lettres = st.slider("Nombre de lettres", min_value=3, max_value=10, value=6)
 
-    # Afficher les prénoms générés en colonnes de 5
-    st.subheader("Prénoms générés")
-    cols = st.columns(5)
-    for i, prenom in enumerate(prenoms_generes):
-        cols[i % 5].write(prenom)
+# Bouton pour générer des prénoms
+if st.button("Générer des prénoms"):
+    if langue == "Français":
+        liste_prenoms = prenoms_francais_m if genre == "Masculin" else prenoms_francais_f
+    elif langue == "Allemand":
+        liste_prenoms = prenoms_allemands_m if genre == "Masculin" else prenoms_allemands_f
+    elif langue == "Anglais":
+        liste_prenoms = prenoms_anglais_m if genre == "Masculin" else prenoms_anglais_f
+    elif langue == "Espagnol":
+        liste_prenoms = prenoms_espagnols_m if genre == "Masculin" else prenoms_espagnols_f
+    elif langue == "Islandais":
+        liste_prenoms = prenoms_islandais_m if genre == "Masculin" else prenoms_islandais_f
+    
+    generated_prenoms = generer_prenom(liste_prenoms, nb_syllabes, nb_lettres)
+    
+    for i in range(0, len(generated_prenoms), 5):
+        cols = st.columns(5)
+        for j, col in enumerate(cols):
+            if i + j < len(generated_prenoms):
+                col.write(generated_prenoms[i + j])
